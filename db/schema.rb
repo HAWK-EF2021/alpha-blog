41cea8b41aacdf08031bd3179132da6bd80cd310
unknown <felipe.abarca@uc.cl>
2021-09-26 19:51:48 -0300

    pene 2

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_26_190149) do
+ActiveRecord::Schema.define(version: 2021_09_26_201531) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2021_09_26_190149) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
 end
